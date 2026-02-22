c3db2a426ca70957b0b2290d782f12b69d16bed6
zariemon <ykpse62493@yahoo.co.jp>
2026-02-22 21:29:49 +0900

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,28 +1,7 @@
-document.getElementById("jsStatus").textContent = "OK";
-document.getElementById("jsStatus").className = "ok";
-
 const pc = new RTCPeerConnection();
-let channel;
 
-// ★ DataChannel を受信
-pc.ondatachannel = ev => {
-  channel = ev.channel;
-  console.log("datachannel open");
-};
-
-// ★ 映像を受信したら表示
 pc.ontrack = e => {
   document.getElementById("remote").srcObject = e.streams[0];
-  document.getElementById("videoStatus").textContent = "OK";
-  document.getElementById("videoStatus").className = "ok";
-};
-
-// ★ WebRTC 接続状態を監視
-pc.onconnectionstatechange = () => {
-  if (pc.connectionState === "connected") {
-    document.getElementById("rtcStatus").textContent = "OK";
-    document.getElementById("rtcStatus").className = "ok";
-  }
 };
 
 async function poll() {
