06b9ccdedd86def086cb542656a78ce9a263dac1
zariemon <ykpse62493@yahoo.co.jp>
2026-02-22 19:28:39 +0900

    Update offer.js

--- api/offer.js
+++ api/offer.js
@@ -1,1 +1,10 @@
+let offer = null;
 
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    offer = JSON.parse(req.body);
+    res.status(200).json({ ok: true });
+  } else {
+    res.status(200).json({ offer });
+  }
+}
